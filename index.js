829d7d24532f05c2c09b5a1ad88a183834797ce7
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-03-30 02:36:02 +0300

    js

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
+//получаю необходимые элементы
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const output = document.getElementById("output");
 
+//создаю функцию с фетч запросом
+function post() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: postTitle.value,
+      body: postText.value,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      let postWrapper = document.createElement("div"); // отрисовка html разметки поста
+      postWrapper.classList.add("post__wrapper");
+      console.log(json.title);
+      postWrapper.innerHTML = `
+          <p class="posted__title">${json.title}</p>
+          <p class="posted__text">${json.body}</p>
+  `;
+      postContainer.appendChild(postWrapper);
+    })
+    .catch((err) => {
+      console.log("Ошибка!" + err);
+    });
+}
+
+//добавляю обработчик событий для кнопки, для добавления поста
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (postTitle.value === "" || postText.value === "") {
+    //проверка на пустоту полей
+    alert("Заполните поле ввода!");
+  } else {
+    postTitle = document.querySelector(".post__title");
+    postText = document.querySelector(".post__text");
+    post();
+  }
+});
